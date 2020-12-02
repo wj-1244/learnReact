af2ca76e49b45d28752b7849ff8611198d85ac48
汪洁 <15151135869@163.com>
2020-12-02 17:58:57 +0800

    高阶组件

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import App from '../src/通信案例/App'
 // import App from '../src/插槽/App'
-import App from '../src/受控和非受控组件/非受控组件的使用'
+import App from '../src/高阶组件的使用/高阶组件的定义方式'
 ReactDOM.render(
     <App />,
   document.getElementById('root')
